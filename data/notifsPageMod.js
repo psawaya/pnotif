49fea795337fcfa3fffd5c5a301759f7e2f15aa8
Paul Sawaya <me@paulsawaya.com>
2011-06-28 12:07:07 -0700

    javascript 101

--- data/notifsPageMod.js
+++ data/notifsPageMod.js
@@ -1,14 +1,14 @@
-// TODO: find a better way of doing this than embedding a global variable.
-var __notifsCallback = function() {};
-window.navigator.pushNotifications = {
-            requestPermissions: function requestPermissions(params,callbackFunc) {
-                __notifsCallback = callbackFunc;
-                window.navigator.pushNotifications['callbackFunc'] = callbackFunc;
-                params['contentHost'] = window.location.host;
-                self.postMessage({ params: params, callbackFunc: callbackFunc });
-            } 
-};
-self.port.on('execNotifsCallback', function(msg) {
-  if (msg && msg.args)
-        __notifsCallback.apply(window,msg.args);
-});
+window.navigator.pushNotifications = (function () {
+            var __notifsCallback = function() {};
+            self.port.on('execNotifsCallback', function(msg) {
+                if (msg && msg.args)
+                    __notifsCallback.apply(window,msg.args);
+            });
+            return {
+                requestPermissions: function requestPermissions(params,callbackFunc) {
+                    __notifsCallback = callbackFunc;
+                    params['contentHost'] = window.location.host;
+                    self.postMessage({ params: params });
+                }
+            }
+    })();
