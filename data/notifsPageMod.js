f8cd4a2cc1ceb0fac38d6bf0f558b271c81a87bf
Paul Sawaya <me@paulsawaya.com>
2011-07-05 10:37:00 -0700

    added new inject.js

--- data/notifsPageMod.js
+++ data/notifsPageMod.js
@@ -1,11 +1,13 @@
-window.navigator.pushNotifications = (function () {
+unsafeWindow.navigator.pushNotifications = (function () {
             var __notifsCallback = function() {};
+            alert(this);
             self.port.on('execNotifsCallback', function(msg) {
                 if (msg && msg.args)
                     __notifsCallback.apply(window,msg.args);
             });
             return {
                 requestPermissions: function requestPermissions(params,callbackFunc) {
+                    alert(__notifsCallback);
                     __notifsCallback = callbackFunc;
                     params['contentHost'] = window.location.host;
                     self.postMessage({ params: params });
